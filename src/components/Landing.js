ea013b408b828be5251aba85ac670706fe9fbc38
gourav <gouravsharma730@gmail.com>
2024-11-28 17:42:33 +0530

    color changes in responsive pages

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import styles from './Landing.module.css';
 import axios from 'axios';
 import { Link } from "react-router-dom";
+import styles from './Landing.module.css';
 
 
 const LandingPage = () => {
